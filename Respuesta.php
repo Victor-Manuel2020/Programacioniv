0668fdd712e4bb88ebdc68af44ceb383f35714b5
MrLuis22 <guevaraenriqueluis22@gmail.com>
2020-03-05 15:45:05 -0600

    Ultimo

---
+++ Respuesta.php
@@ -0,0 +1,14 @@
+<?php
+
+if($_GET && isset($_GET['A']) && isset($_GET['DE']) && isset($_GET['Cantidad'])){
+    $A = $_GET['A'];
+    $DE = $_GET['DE'];
+    $Cantidad = $_GET['Cantidad'];
+
+    echo "la respuesta de:".round((($A/$DE)*$Cantidad),2);
+
+}else{
+   header("location: index.html");
+}
+
+?>
